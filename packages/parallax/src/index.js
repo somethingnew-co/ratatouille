989643a04cf8e3845e5bddbd8f8c81105ba45b9b
Jens Fischer <jens@weareprofound.com>
2019-03-01 18:28:20 -0500

    contentful proxy wip

--- packages/parallax/src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Parallax } from './components/Parallax';
-export { default as ParallaxProvider } from './components/ParallaxProvider';
-export { default as ParallaxBanner } from './components/ParallaxBanner';
-export { default as ParallaxController } from './libs/ParallaxController';
--- packages/parallax/src/utils/index.js
+++
@@ -1,6 +0,0 @@
-export { default as clamp } from './clamp';
-export { default as getParallaxOffsets } from './getParallaxOffsets';
-export { default as isElementInView } from './isElementInView';
-export { default as parseValueAndUnit } from './parseValueAndUnit';
-export { default as scaleBetween } from './scaleBetween';
-export { default as testForPassiveScroll } from './testForPassiveScroll';
