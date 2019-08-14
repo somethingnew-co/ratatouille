b12edd94b06c2a8670c92ae012c7409c13e6ff1f
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2019-08-14 13:01:35 -0400

    remove babel, fix build scripts (#4)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,12 +4,11 @@ module.exports = ({ config, mode }) => {
       exclude: /(node_modules)/,
       use: [
         {
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [['react-app', { flow: false, typescript: true }]],
-          },
+          loader: require.resolve('awesome-typescript-loader'),
         },
-        { loader: require.resolve('react-docgen-typescript-loader') },
+        {
+          loader: require.resolve('react-docgen-typescript-loader')
+        },
       ],
     });
     config.resolve.extensions.push('.ts', '.tsx');
