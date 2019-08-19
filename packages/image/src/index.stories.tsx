d2e3527a918f3c4410eade69b622cda8ab6dd623
Sam Kilgus <samkilgus@gmail.com>
2019-08-19 15:36:13 -0400

    added responsive-image, updated story for image (#9)
    * added responsive-image, updated story for image
    
    * updated augmented interface, fixes...
    
    * removed dependency

--- packages/image/src/index.stories.tsx
+++ packages/image/src/index.stories.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
 
 import Image from '.';
 
 const ImageStories = storiesOf('Image', module);
 
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 100%;
+  max-height: 100%;
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+`;
+
 ImageStories.add('default', () => (
-  <Image src={'/assets/stn-new.png'} />
+  <Container>
+    <Image src={'/assets/stn-new.png'} />
+  </Container>
 ));
---
+++ packages/responsiveImage/src/index.tsx
@@ -0,0 +1,119 @@
+import React from 'react';
+
+declare module 'react' {
+  interface ImgHTMLAttributes<T> {
+    loading?: 'lazy' | 'eager' | 'auto';
+  }
+}
+
+interface ResponsiveImageProps {
+  sources: {
+    src: string;
+    width: number;
+    isDefault?: boolean;
+  }[];
+  indexBy?: 'min-width' | 'max-width';
+  indexUnit?: 'px' | 'rem' | 'vw';
+  lazyTimeout?: number;
+  lazy?: boolean;
+  alt?: string;
+  title?: string;
+}
+interface ResponsiveImageState {
+  loaded: boolean;
+}
+
+class ResponsiveImage extends React.Component<ResponsiveImageProps, ResponsiveImageState> {
+  static defaultProps: Partial<ResponsiveImageProps> = {
+    lazy: false,
+    lazyTimeout: 2000,
+    indexBy: 'min-width',
+    indexUnit: 'px',
+  };
+
+  imageElement: React.RefObject<HTMLImageElement>;
+
+  observer: IntersectionObserver | null;
+
+  constructor(props: ResponsiveImageProps) {
+    super(props);
+    const { lazy } = props;
+
+    this.state = { loaded: !lazy };
+
+    this.imageElement = React.createRef();
+    this.observer = null;
+
+    this.completeLoad = this.completeLoad.bind(this);
+    this.interactionHandler = this.interactionHandler.bind(this);
+  }
+
+  componentDidMount(): void {
+    const { lazy } = this.props;
+    if (!lazy) {
+      this.observer = new IntersectionObserver(this.interactionHandler, {
+        rootMargin: '500px',
+      });
+      if (this.imageElement.current) this.observer.observe(this.imageElement.current);
+      setTimeout(this.completeLoad, 2000);
+    }
+  }
+
+  componentWillUnmount(): void {
+    if (this.observer) this.observer.disconnect();
+  }
+
+  interactionHandler(entries: IntersectionObserverEntry[]): void {
+    if (entries[0].isIntersecting) this.completeLoad();
+  }
+
+  completeLoad(): void {
+    const { loaded } = this.state;
+    if (!loaded && !!this.imageElement.current) {
+      this.setState({ loaded: true }, () => {
+        if (this.observer)
+          this.observer.unobserve(this.imageElement.current as Element);
+      });
+    }
+  }
+
+  render(): JSX.Element {
+    const { sources, alt, title, lazy, indexBy, indexUnit } = this.props;
+    const { loaded } = this.state;
+
+    const defaultMedia = sources
+      .filter(source => source.isDefault && source.src)[0]
+      || sources.filter(source => source.src)[0];
+
+    const otherMedia = sources
+      .filter(source => source.src);
+
+    if (loaded && defaultMedia) {
+      return (
+        <picture ref={this.imageElement}>
+          {otherMedia.map(media => (
+            <source
+              key={media.src}
+              media={`(${indexBy}: ${media.width}${indexUnit})`}
+              srcSet={media.src}
+            />
+          ))}
+          <img
+            src={defaultMedia.src}
+            alt={alt || title}
+            title={title || alt}
+            loading={lazy ? 'lazy' : 'auto'}
+          />
+        </picture>
+      );
+    }
+
+    return (
+      <picture ref={this.imageElement}>
+        <img alt={alt || title} title={alt ? title : ''} />
+      </picture>
+    );
+  }
+}
+
+export default ResponsiveImage;
