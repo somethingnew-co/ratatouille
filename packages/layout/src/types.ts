97c94f69fa79e1a3081c14668e2972bda9ff13e1
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2019-08-20 18:32:10 -0400

    add further documentation

--- packages/layout/src/types.ts
+++ packages/layout/src/types.ts
@@ -1,9 +1,19 @@
+/* eslint-disable @typescript-eslint/indent */
 import { StyledComponentProps } from 'styled-components';
 
 /**
  * Prop types for functions that return a themed styled component.
- * satisfys "as" prop warnings, but loose typings means a lot of
+ * Satisfies "as" prop warning, but loose typings means a lot of
  * attributes are passed through.
  */
-type ComponentStyledProps = StyledComponentProps<keyof JSX.IntrinsicElements | React.ComponentType<any>, object, object, any>
-export type SC = ComponentStyledProps
+type ComponentWithStyledProps = StyledComponentProps<
+  // The Component from whose props are derived
+  keyof JSX.IntrinsicElements | React.ComponentType<any>,
+  // The Theme from the current context
+  object,
+  // The other props added by the template
+  object,
+  // The props that are made optional by .attrs
+  any
+>
+export type SC = ComponentWithStyledProps
