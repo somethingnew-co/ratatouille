1b4ba556318e61020396ed23ec9edaa81797204d
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2019-09-09 10:46:41 -0400

    fix gap and max-width on Container (#19)

--- packages/layout/src/Container.tsx
+++ packages/layout/src/Container.tsx
@@ -1,26 +1,33 @@
-/**
- * <Container>
- * Wrapper component based on styled-components theme
- */
-
 import React, { FC } from 'react';
 import { ThemeContext } from 'styled-components';
+import { makePropArray } from './helpers';
 import { SC } from './types';
-
 import { Box } from './Box';
 
+// <Container>
 /**
  * Extension of `<Box>` with max-width applied that corresponds to theme breakpoints.
  */
 export const Container: FC<SC> = props => {
   const theme = React.useContext(ThemeContext);
+  const gap = theme ? makePropArray(theme.grid.gap).map(gap => `${gap / 2}px`) : '10px';
+
+  let maxWidth;
+
+  if (theme && Array.isArray(theme.grid.maxWidth)) {
+    maxWidth = ['100%', ...theme.grid.maxWidth];
+  } else if (theme && theme.grid.maxWidth) {
+    ({ maxWidth } = theme.grid);
+  } else {
+    maxWidth = 1000;
+  }
 
   return (
     <Box
       mx="auto"
-      px={theme ? theme.grid.gap / 2 : '10px'}
+      px={gap}
       width="100%"
-      maxWidth={theme ? theme.breakpoints : 1000}
+      maxWidth={maxWidth}
       {...props}
     >{props.children}</Box>
   );
