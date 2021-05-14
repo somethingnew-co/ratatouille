63c6d0485bad141352b0a72c7f594fc3c9675a23
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-05-14 14:12:00 -0400

    Lint

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -4,9 +4,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     '@stnew',
   ],
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
     'indent': 'off',
     'no-undef': 'off',
@@ -15,22 +13,28 @@ module.exports = {
     '@typescript-eslint/indent': ['error', 2],
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-member-accessibility': ['error', {
-      accessibility: 'no-public',
-    }],
-    '@typescript-eslint/member-delimiter-style': ['error', {
-      multiline: {
-        delimiter: 'none',
-        requireLast: true,
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error', {
+        accessibility: 'no-public',
       },
-      singleline: {
-        delimiter: 'comma',
-        requireLast: false,
+    ],
+    '@typescript-eslint/member-delimiter-style': [
+      'error', {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false,
+        },
       },
-    }],
-    '@typescript-eslint/explicit-function-return-type': ['error', {
-      allowExpressions: true,
-      allowTypedFunctionExpressions: true,
-    }],
+    ],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error', {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+      },
+    ],
   },
 };
